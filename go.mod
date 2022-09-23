d17933724306921b4b18729236e97d08dc77e944
jolheiser <john.olheiser@gmail.com>
2022-09-23 11:07:51 -0500

    Upgrade to Chroma 2.2.0
    Adjust the tests to account for the change PreventSurroundingPre()
    
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yuin/goldmark-highlighting/v2
 go 1.13
 
 require (
-	github.com/alecthomas/chroma v0.10.0
+	github.com/alecthomas/chroma/v2 v2.1.0
 	github.com/yuin/goldmark v1.4.15
+
 )
--- highlighting.go
+++ highlighting.go
@@ -18,10 +18,10 @@ import (
 	"github.com/yuin/goldmark/text"
 	"github.com/yuin/goldmark/util"
 
-	"github.com/alecthomas/chroma"
-	chromahtml "github.com/alecthomas/chroma/formatters/html"
-	"github.com/alecthomas/chroma/lexers"
-	"github.com/alecthomas/chroma/styles"
+	"github.com/alecthomas/chroma/v2"
+	chromahtml "github.com/alecthomas/chroma/v2/formatters/html"
+	"github.com/alecthomas/chroma/v2/lexers"
+	"github.com/alecthomas/chroma/v2/styles"
 )
 
 // ImmutableAttributes is a read-only interface for ast.Attributes.
